27831c7e0273574d0c8bfa06e2e127b43451f124
NickSwainston <nickaswainston@gmail.com>
2024-02-16 20:33:57 +1100

    A test util to assert floats are within six digits

---
+++ tests/lib/TestUtils.groovy
@@ -0,0 +1,13 @@
+class TestUtils {
+
+    def static floats_within_six_digits(Float float1, Float float2){
+      def precision = 6;
+
+      // Use an epsilon value based on the desired precision
+      def epsilon = Math.pow(10, -precision)
+
+      // Compare the absolute difference to the epsilon value
+      return Math.abs(float1 - float2) < epsilon
+    }
+
+}
